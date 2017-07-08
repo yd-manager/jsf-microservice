7491a42d4b2511b18dc5773babf57ab84d3acd7b
ycakar <ycakar@netas.com.tr>
2017-07-09 00:31:08 +0300

    spring boot rest

--- rest-project/rest-api/src/main/java/tr/com/yd/tabak/restapi/app/RestApiApp.java
+++ rest-project/rest-api/src/main/java/tr/com/yd/tabak/restapi/app/RestApiApp.java
@@ -1,5 +1,12 @@
 package tr.com.yd.tabak.restapi.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages={"tr.com.yd.tabak.restapi.dao","tr.com.yd.tabak.restapi.service"})
 public class RestApiApp {
 
+		public static void main(String[] args) {
+			SpringApplication.run(RestApiApp.class, args);
+		}
 }
--- rest-project/rest-api/src/main/java/tr/com/yd/tabak/restapi/rest/UserRestController.java
+++ rest-project/rest-api/src/main/java/tr/com/yd/tabak/restapi/rest/UserRestController.java
@@ -1,5 +1,8 @@
 package tr.com.yd.tabak.restapi.rest;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserRestController extends BaseRestController {
 
 	
